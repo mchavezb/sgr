2c84134631eb3d6db1dca576b7fc3036ec683019
Marco <marcochavezb@gmail.com>
2013-09-05 01:35:26 -0500

    pagina de inicio

--- system/application/views/index.php
+++ system/application/views/index.php
@@ -1,1 +1,11 @@
-Hola!
+<?php setlocale(LC_TIME,"es_ES", "es_ES.utf8");?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Inicio | SGR</title>
+  </head>
+  <body>
+    Página de inicio del Sistema de Gestión de Restaurantes
+  </body>
+</html>
