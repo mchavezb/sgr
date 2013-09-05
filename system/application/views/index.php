bf11dc29f7752e0a63ca36ce58a9ef9383f464ad
Marco <marcochavezb@gmail.com>
2013-09-05 01:50:34 -0500

    index.php login

--- system/application/views/index.php
+++ system/application/views/index.php
@@ -4,8 +4,37 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Inicio | SGR</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
-    Página de inicio del Sistema de Gestión de Restaurantes
+
+    <div class="container">
+
+      <form class="form-signin" action="administracion/index.html" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="input-block-level" placeholder="Usuario">
+        <input type="password" class="input-block-level" placeholder="Contrasena">
+        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
   </body>
 </html>
