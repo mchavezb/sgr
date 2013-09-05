2a5d9d399697e73cd8bb6bd30dcf3bb23e46609a
Marco <marcochavezb@gmail.com>
2013-09-05 02:03:16 -0500

    files css img js

--- system/application/views/index.php
+++ system/application/views/index.php
@@ -59,7 +59,7 @@
       <form class="form-signin" action="administracion/index.html" method="post">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input type="text" class="input-block-level" placeholder="Usuario">
-        <input type="password" class="input-block-level" placeholder="Contrasena">
+        <input type="password" class="input-block-level" placeholder="Contraseña">
         <button class="btn btn-large btn-primary" type="submit">Sign in</button>
       </form>
 
