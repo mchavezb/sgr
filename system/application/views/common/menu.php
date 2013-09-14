288ff25cc385050834d240fd39a362d07b671956
lramirez <lauraramirez988@gmail.com>
2013-09-13 21:58:16 -0500

    sgr

---
+++ system/application/views/common/menu.php
@@ -0,0 +1,21 @@
+<div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Panel de Control</li>
+			  <li class="nav-header">Mesas</li>
+              <li><a href="../mesas/index.php">Ver Mesas</a></li>
+              <li class="nav-header">Ventas</li>
+              <li><a href="../ventas/pedidos.php">Pedidos</a></li>
+              <li><a href="../ventas/deliverys.php">Deliverys</a></li>
+              <li><a href="../ventas/reservas.php">Reservas</a></li>
+              <li class="nav-header">Contabilidad</li>
+              <li><a href="../contabilidad/apertura.php">Apertura de Caja</a></li>
+              <li><a href="../contabilidad/arqueo.php">Arqueo de Caja</a></li>
+              <li><a href="../contabilidad/flujo.php">Flujo de Efectivo</a></li>
+			  <li class="nav-header">Administracion</li>
+              <li><a href="mesas.php">Mesas</a></li>
+              <li><a href="platos.php">Platos</a></li>
+              <li><a href="usuario.php">Usuarios</a></li>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
