066c8d45e4bf4d098c9ebc06f828b08668023c29
lramirez <lauraramirez988@gmail.com>
2013-09-14 10:42:11 -0500

    sgr

--- system/application/models/m_usuarios.php
+++ system/application/models/m_usuarios.php
@@ -1,8 +1,8 @@
 <?php
 class Usuarios_model extends CI_Model{
    function ValidarUsuario($email,$password){         
-      $query = $this->db->where('Usuario',$email);   
-      $query = $this->db->where('Password',$password);
+      $query = $this->db->where('usuario',$email);   
+      $query = $this->db->where('password',$password);
       $query = $this->db->get('Usuarios');
       return $query->row();   
    }
