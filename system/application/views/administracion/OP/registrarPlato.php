cef3b570a49f9860a15a775775434f15aa850945
lramirez <lauraramirez988@gmail.com>
2013-09-13 22:35:37 -0500

    sgr

--- system/application/views/administracion/OP/registrarPlato.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once('../DB/platoDB.php');
-
-
-	$result = Platos::insert( $_POST['nombre'] , $_POST['estado'], $_POST['tiempo'], $_POST['precio'] );
-
-
-	header('Location: http://sgr.host56.com/administracion/platos.php');
-?>
