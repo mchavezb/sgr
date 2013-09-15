f0e56b140a0c6572228712c2866eabfe5eab44a8
Marco <marcochavezb@gmail.com>
2013-09-14 21:19:05 -0500

    inicio

--- system/application/controllers/index.php
+++ system/application/controllers/index.php
@@ -5,34 +5,10 @@ class Index extends CI_Controller {
 	public function __construct()
  	{
   	parent::__construct();
-	$this->load->helper('form_validation');
-	$this->load->model('m_usuarios');
  	}
  	
 	public function index()
 	{
 		$this->load->view('index');
-		
-		if(!isset($_POST['usuario'])){     
-			$this->load->view('index');     
-		}
-		else{                        
-			$this->form_validation->set_rules('usuario','required|valid_email');      
-			$this->form_validation->set_rules('password','password','required');
-			if(($this->form_validation->run()==FALSE)){           
-				$this->load->view('index');                    
-			}
-			else{                                      
-				$this->load->model('usuarios_model');
-				$ExisteUsuarioyPassoword=$this->usuarios_model->ValidarUsuario($_POST['usuario'],$_POST['password']);   
-				if($ExisteUsuarioyPassoword){   
-					$this->load->view('mesas/index');   
-				}
-				else{   
-					$data['error']="Usuario o password incorrecto, por favor vuelva a intentar";
-					$this->load->view('login',$data);  
-				}
-			}
-		}
 	}
 }
--- system/application/views/index.php
+++ system/application/views/index.php
@@ -1,79 +1,8 @@
-<<<<<<< HEAD
-=======
 <?php setlocale(LC_TIME,"es_ES", "es_ES.utf8");?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Inicio | SGR</title>
-    <link href="files/css/bootstrap.css" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-      body {
-        padding-top: 40px;
-        padding-bottom: 40px;
-        background-color: #f5f5f5;
-      }
-
-      .form-signin {
-        max-width: 300px;
-        padding: 19px 29px 29px;
-        margin: 0 auto 20px;
-        background-color: #fff;
-        border: 1px solid #e5e5e5;
-        -webkit-border-radius: 5px;
-           -moz-border-radius: 5px;
-                border-radius: 5px;
-        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                box-shadow: 0 1px 2px rgba(0,0,0,.05);
-      }
-      .form-signin .form-signin-heading,
-      .form-signin .checkbox {
-        margin-bottom: 10px;
-      }
-      .form-signin input[type="text"],
-      .form-signin input[type="password"] {
-        font-size: 16px;
-        height: auto;
-        margin-bottom: 15px;
-        padding: 7px 9px;
-      }
-
-    </style>
-        <link href="files/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="../files/js/html5shiv.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="files/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="files/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="files/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="files/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="files/ico/favicon.png">
   </head>
-
   <body>
-
-    <div class="container">
-
-      <form class="form-signin" method="post" action="administracion/index.html">
-    <img src="files/img/logo.png" alt="">
-        <h2 class="form-signin-heading">SGR</h2>
-        <input type="text" class="input-block-level" name="usuario" placeholder="Usuario">
-        <input type="password" class="input-block-level" name="password" placeholder="Contraseña">
-        <button class="btn btn-large btn-primary" type="submit">Iniciar Sesión</button>
-      </form>
-
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="files/js/jquery.js"></script>
-
   </body>
 </html>
->>>>>>> 066c8d45e4bf4d098c9ebc06f828b08668023c29
