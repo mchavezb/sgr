a6dd79bf774026f38fa5bca2da72fc89bddee230
Marco <marcochavezb@gmail.com>
2013-09-17 00:46:36 -0500

    mesas

--- system/application/controllers/comanda.php
+++ system/application/controllers/comanda.php
@@ -5,11 +5,14 @@ class Comanda extends CI_Controller {
 	public function __construct()
  	{
   	parent::__construct();
+  	$this->load->database();
+  	$this->load->model('comanda_mo');
  	}
 
 	public function m($id)
 	{
 		$data['id'] = $id;
+		$data['info_comanda'] = $this->comanda_mo->get_comanda_by_table($id);
 		$this->load->view('comanda_vw',$data);
 	}
 }
