5a72d63bd770a9d8db0f98d6ab20f43fec9a9ac1
lramirez <lauraramirez988@gmail.com>
2013-11-13 06:56:00 -0500

    adm

--- system/application/views/ventas/reservas.php
+++ system/application/views/ventas/reservas.php
@@ -39,19 +39,23 @@
                 <td style="background-color:gray; border-radius:5px; height:18px; width: 150px; text-align:center;">Cliente</td>
                 </tr>
               
-            
-                <?php foreach ($reservas->result() as $row) {
-                echo '<tr>';
-                echo '<td>'.$row->idReserva.'</td>';
-                echo '<td>'.$row->Mesa_idMesa.'</td>';
-                echo '<td>'.$row->fecha.'</td>';
-                echo '<td>'.$row->numero_personas.'</td>';
-                echo '<td>'.$row->nombre_cliente.'</td>';
-                echo '<td><a href="'.base_url().'reservas/modificarReserva/'.$row->idReserva.'" class="btn btn-info">Modificar</a><a href="" class="btn btn-danger"><i class="icon-trash icon-white"></i></a></td>';
-                echo '<td><a href="'.base_url().'reservas/cancelarReserva/'.$row->idReserva.'" class="btn btn-info">Cancelar</a><a href=""  class="btn btn-danger"><i class="icon-trash icon-white"></i></a></td>';
-                echo '</tr>';
-            
-               }?>
+                
+                  <?php 
+                  if($reservas==false){
+                    echo "<p>No hay reservas pendientes</p>";
+                  }else{
+                  foreach ($reservas->result() as $row) {
+                
+                    echo '<tr>';
+                    echo '<td>'.$row->idReserva.'</td>';
+                    echo '<td>'.$row->Mesa_idMesa.'</td>';
+                    echo '<td>'.$row->fecha.'</td>';
+                    echo '<td>'.$row->numero_personas.'</td>';
+                    echo '<td>'.$row->nombre_cliente.'</td>';
+                    echo '<td><a href="'.base_url().'reservas/modificarReserva/'.$row->idReserva.'" class="btn btn-info">Modificar</a><a href="" class="btn btn-danger"><i class="icon-trash icon-white"></i></a></td>';
+                    echo '<td><a href="'.base_url().'reservas/cancelarReserva/'.$row->idReserva.'" class="btn btn-info">Cancelar</a><a href=""  class="btn btn-danger"><i class="icon-trash icon-white"></i></a></td>';
+                    echo '</tr>';
+                }}?>
               
             </table>
           </div>
